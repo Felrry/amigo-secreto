8bbbe450f41300ecbbf3567824d121435d5b2790
Felrry <luismg_19@hotmail.com>
2025-01-24 21:55:39 -0400

    Agregando la funcion limpiarCaja

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //creando array
-
 let amigos = [];
 
-//Declarando la funcion agregar amigo
 
+//Declarando la funcion agregar amigo
 function agregarAmigo() {
     //Agrega los nombres de los amigos y los almacena en el array amigos
     let entradaNombreDeAmigo = document.getElementById('amigo').value;
@@ -17,8 +16,8 @@ function agregarAmigo() {
     }
     
 }
-
-function limpiarCajas(){
+function limpiarCajas() {
     //Limpia la caja de texto
     document.getElementById('amigo').value = '';
+    
 }
