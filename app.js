856173ecb31e5072454784f50a6df56294e39df9
Felrry <luismg_19@hotmail.com>
2025-01-27 18:41:15 -0400

    actualizando comentarios y funciones

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ let amigos = [];
 function agregarAmigo() {
     //Agrega los nombres de los amigos y los almacena en el array amigos
     let entradaNombreDeAmigo = document.getElementById("amigo");
-    if (entradaNombreDeAmigo == "") {
+    if (entradaNombreDeAmigo.value == "" || entradaNombreDeAmigo.value >= 0 || entradaNombreDeAmigo.value <= 0) { 
+        //si funciona no lo toques
         alert("Por favor, inserta un nombre.");
+        limpiarCajas();
     } else {
         amigos.push(entradaNombreDeAmigo.value);
         amigos.sort();
-        console.log(amigos);
         limpiarCajas();
         actualizarListaDeAmigos();
     }
@@ -45,11 +46,11 @@ function sortearAmigo() {
     if (amigos == "") { //Comprobando si el array amigos esta vacio
         agregarAmigo();
     } else {
-        let cantidadDeNombres = amigos.length;
-        let amigoSeleccionado = amigos[Math.floor(Math.random() * cantidadDeNombres)];
-        let resultado = document.getElementById('resultado')
-        resultado.innerHTML = `El amigo secreto seleccionado es: ${amigoSeleccionado}`;
-        document.getElementById("listaAmigos").innerHTML = "";
+        let cantidadDeNombres = amigos.length; //comprobando la cantidad de elementos en el array amigos
+        let amigoSeleccionado = amigos[Math.floor(Math.random() * cantidadDeNombres)]; // determinando el amigo seleccionado
+        let resultado = document.getElementById('resultado') 
+        resultado.innerHTML = `El amigo secreto seleccionado es: ${amigoSeleccionado}`; // presentando el amigo seleccionado
+        document.getElementById("listaAmigos").innerHTML = ""; //eliminando los nombres de la etiqueta listaAmigos
     }
     return;
 }
